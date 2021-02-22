577e0af6f0e716d5544a75af5b3b0741d8cb5d46
r.kaminin <r.kaminin@lancelot-it.ru>
2021-02-22 18:09:15 +0300

    fix tests

--- src/test/java/ru/example/resttestapp/controllers/OrderControllerTest.java
+++ src/test/java/ru/example/resttestapp/controllers/OrderControllerTest.java
@@ -14,20 +14,19 @@ import org.springframework.test.web.servlet.RequestBuilder;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(OrderController.class)
 public class OrderControllerTest {
 
-
     @Autowired
     private MockMvc mockMvc;
 
-    private String exampleOrderJson = "{\"seller\":\"999999999\",\"customer\":\"999999999\",\"products\":[{\"name\":\"milk\",\"code\":\"1111111111111\"}]}";
-    private String errorOrderJson = "{\"seller\":\"111111111\",\"customer\":\"11\",\"products\":[{\"name\":\"milk\",\"code\":\"1111111111111\"}]}";
-
     @Test
     public void createOrder() throws Exception {
+        String exampleOrderJson = "{\"seller\":\"999999999\",\"customer\":\"999999999\",\"products\":[{\"name\":\"milk\",\"code\":\"1111111111111\"}]}";
         RequestBuilder requestBuilder = post("/api/order")
                 .accept(MediaType.APPLICATION_JSON).content(exampleOrderJson)
                 .contentType(MediaType.APPLICATION_JSON);
@@ -39,15 +38,15 @@ public class OrderControllerTest {
 
     @Test
     public void orderWithNotCorrectFields() throws Exception {
-//        String expectedJson = "[{\"field\":\"customer\",\"value\":\"11\",\"products\":\"The length must be equal 9\"}]";
+        String expectedJson = "[{\"field\":\"customer\",\"value\":\"11\",\"message\":\"The length must be equal 9\"}]";
+        String requestWithErrors = "{\"seller\":\"111111111\",\"customer\":\"11\",\"products\":[{\"name\":\"milk\",\"code\":\"1111111111111\"}]}";
         RequestBuilder requestBuilder = post("/api/order")
                 .accept(MediaType.APPLICATION_JSON)
-                .content(errorOrderJson)
+                .content(requestWithErrors)
                 .contentType(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        MockHttpServletResponse response = result.getResponse();
-        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatus());
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(expectedJson));
     }
 
 }
